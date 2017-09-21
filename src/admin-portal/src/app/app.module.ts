0e09b705c894f08a5d08fe8480d94a46e6673923
andreimerfu <andrei.merfu@my.fmi.unibuc.ro>
2017-09-21 11:43:56 +0300

    Initial commit

---
+++ src/admin-portal/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { LoginService } from './services/login.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    LoginService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
