b852b721acf878d066ff8396d30357c0f1f2db1b
andreimerfu <andrei.merfu@my.fmi.unibuc.ro>
2017-10-04 20:30:04 +0300

    Login / Create accout works + email sent.

--- src/store-front/src/app/components/nav-bar/nav-bar.component.ts
+++ src/store-front/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  private loggedIn = false;
+
+  constructor(
+    private LoginService: LoginService
+  ) { }
+
+  logout(){
+    this.LoginService.logout().subscribe(
+      res => {
+        location.reload();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit() {
+    this.LoginService.checkSession().subscribe(
+      res => {
+        this.loggedIn = true;
+      },
+      error => {
+        this.loggedIn = false;
+      }
+    );
   }
 
 }
